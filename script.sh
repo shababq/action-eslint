2ab2ba8ef3424645b815160dd30a0ad33a50b153
shababq <99688125+shababq@users.noreply.github.com>
2023-02-21 15:53:34 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo '::endgroup::'
 
 if [ ! -f "$(npm bin)/eslint" ]; then
   echo '::group:: Running `npm install` to install eslint ...'
-  npm install
+  npm install --force --legacy-peer-deps
   echo '::endgroup::'
 fi
 
